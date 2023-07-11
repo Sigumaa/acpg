f6178eeace1ad2ce27485c2ba71d33396b3db0d7
shiyui <66453922+Sigumaa@users.noreply.github.com>
2023-07-11 21:09:30 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-var Url int
+type A struct {
+	Url string
+	Id  string
+	Utc string
+}
 
 func main() {
-  var Url string
-  print(Url)
+	var Url string
+	print(Url)
+
+	var Id string
+	print(Id)
+
+	var Utc string
+	print(Utc)
 }
