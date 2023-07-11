fe8310d6c9b620455fa2b4384026556c2ac13469
shiyui <66453922+Sigumaa@users.noreply.github.com>
2023-07-11 21:13:53 +0900

    Create error.go

---
+++ error.go
@@ -0,0 +1,18 @@
+package main
+
+type E struct {
+	Url string
+	Id  string
+	Utc string
+}
+
+func err() {
+	var Url string
+	print(Url)
+
+	var Id string
+	print(Id)
+
+	var Utc string
+	print(Utc)
+}
